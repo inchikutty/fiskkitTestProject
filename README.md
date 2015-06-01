f193a1689648f331b0f1819d322e8747282d48e9
inchikutty <inchikutty13@gmail.com>
2015-06-01 18:31:36 +0530

    Project directory generated using cg-angular
    Generated project files/structure using generator-cg-angular

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fiskkitTestProject
+
 Test Project for fiskkit Generated using cg-angular
+
+Task: Create a simple app that allows you to enter a url and click a button. 
+The button should show and hide an overlay containing the website of the url on the same page.
+User should be able to input any url and change the displayed page.
---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('fiskkitTestProject', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('fiskkitTestProject').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('fiskkitTestProject').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
